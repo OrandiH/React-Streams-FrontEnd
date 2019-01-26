edca3be5db3d24730401901731383a3a2fae62b2
Orandi Harris <orharrisqw@Orandi-Harris-MacBook-Pro.local>
2019-01-25 22:41:02 -0500

    Initial components and configurations completed

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+//Use Link instead of anchor tag for navigation
+
+
+const App = () => {
+  // Do not use anchor tags with React router
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+};
+
+export default App;
