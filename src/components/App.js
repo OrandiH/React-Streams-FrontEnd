c435a55744208785ce7e0d85a08adda6690e476e
Orandi Harris <orharrisqw@Orandi-Harris-MacBook-Pro.local>
2019-02-27 20:21:10 -0500

    Added new files for stream delete,edit and show

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 //Use Link instead of anchor tag for navigation
 
@@ -14,16 +15,18 @@ const App = () => {
   // Do not use anchor tags with React router
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact render={() => <StreamCreate />} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
-          <Route path="/streams/delete" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
+          <Switch>
+              <Route path="/" exact component={StreamList} />
+              <Route path="/streams/new" exact render={() => <StreamCreate />} />
+              <Route path="/streams/edit/:id" exact component={StreamEdit} />
+              <Route path="/streams/delete/:id" exact component={StreamDelete} />
+              <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 };
